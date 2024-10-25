798a3d83436c052300787a933a9641e7ab8da3e0
JeonJun202 <jjg010202@gmail.com>
2024-10-25 15:37:41 +0900

    전준기 자기소개

---
+++ src/전준기/FirstImpression.js
@@ -0,0 +1,13 @@
+export default function FirstImpression() {
+  return (
+    <div className="wrap">
+      <h3>팀원 첫인상</h3>
+      <ul className="noDot">
+        <li>수환 님 : 일 잘하실 것 같은 느낌..! </li>
+        <li>동재 님 : 활발하시구 열정적인 느낌..!</li>
+        <li>신혜 님 : 장난끼 많으신 느낌..!</li>
+        <li>현묵 님 : 으른 같은 느낌..!</li>
+      </ul>
+    </div>
+  );
+}
---
+++ src/전준기/GrowUp.js
@@ -0,0 +1,16 @@
+export default function GrowUp() {
+  return (
+    <div className="wrap">
+      <h3>성장과정</h3>
+      <div className="grid">
+        <div>22년 여름 첫 java 입문</div>
+        <div>
+          22년 겨울 java gui로 <br />
+          틀린그림찾기 게임 만들기
+        </div>
+        <div>23년 소프트웨어 복전 시작</div>
+        <div>24년 1학기 끝나고 코드잇 부트캠프 시작</div>
+      </div>
+    </div>
+  );
+}
