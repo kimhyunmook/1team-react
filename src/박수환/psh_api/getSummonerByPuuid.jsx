36330cedd7aac171a34677e6c29f3797f451441a
shpark <68580778+soohwanpak@users.noreply.github.com>
2024-10-25 17:45:49 +0900

    Update getSummonerByPuuid.jsx
    Update getSummonerByPuuid.jsx

--- src/박수환/psh_api/getSummonerByPuuid.jsx
+++ src/박수환/psh_api/getSummonerByPuuid.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getSummonerByPuuid = async (puuid) => {
-    const api_key = "RGAPI-bf62ab80-4906-43b4-8575-9e7505f2495d";
+    const api_key = "";
     try {
         const response = await axios.get(`https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-puuid/${puuid}?api_key=${api_key}`);
         return response.data;
