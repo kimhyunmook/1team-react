ad60b6efcfe4c20bc0a8891543d8205cb3c82d05
shpark <68580778+soohwanpak@users.noreply.github.com>
2024-10-25 17:45:09 +0900

    Update getChampionMastery.jsx
    Update getChampionMastery.jsx

--- src/박수환/psh_api/getChampionMastery.jsx
+++ src/박수환/psh_api/getChampionMastery.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const getChampionMastery = async (puuid) => {
-    const api_key = "RGAPI-bf62ab80-4906-43b4-8575-9e7505f2495d";
+    const api_key = "";
     try {
         const response = await axios.get(`https://kr.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-puuid/${puuid}?api_key=${api_key}`);
         return response.data;
