14d4b6945093130cebe55492367a1dcd33f91bec
Kimhyunmook <gusanr4200@naver.com>
2024-10-25 16:02:09 +0900

    .

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,23 @@
+import { Link } from "react-router-dom";
 export default function HomePage() {
   return (
     <div className="home">
-      <h1>여긴 홈페이지</h1>
+      <ul>
+        <List title={"김현묵"} href={"/khm"} />
+        <List title={"박수환"} href={"/psh"} />
+        <List title={"이동재"} href={"/"} />
+        <List title={"전준기"} href={"/"} />
+        <List title={"하신혜"} href={"/"} />
+      </ul>
     </div>
+  );
+}
+
+function List({ href, title }) {
+  return (
+    <li>
+      <div className="door"></div>
+      <Link to={href}>{title}</Link>
+    </li>
   );
 }
