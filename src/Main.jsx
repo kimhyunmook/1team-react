f975b263d67a5f948091ad7c5aa3ddf44b315f44
shpark <68580778+soohwanpak@users.noreply.github.com>
2024-10-25 12:24:43 +0900

    Update Main.jsx
    Main.jsx파일 수정

--- src/Main.jsx
+++ src/Main.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/home";
 import Khm from "./pages/khm/khm";
+import ParkSooHwan from "./pages/ParkSooHwan";
 
 function Main() {
   return (
@@ -9,6 +10,7 @@ function Main() {
         {/* 여기에 Route 추가 */}
         <Route path="/" element={<HomePage />} />
         <Route path="/khm" element={<Khm />} />
+        <Route path="/psh" element={<ParkSooHwan />} />
       </Routes>
     </BrowserRouter>
   );
